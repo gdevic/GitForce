db2cebafd128899d9fb9707c76b5709c26817124
Goran Devic <GitForce.Project@gmail.com>
2011-10-22 00:01:08 -0500

    ### Release 1.0.8 (2011/10/22, 00:00:04)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Custom tool macro code change
    simple reshuffle.
    
    Add new macros to the custom tool:
    %u -> current user name
    %e -> current user email address
    %b -> current branch
    %f -> single selected file
    %F -> list of selected files
    %d -> single selected directory
    %D -> list of selected directories
    
    Suggested by Pau S.
    
    Added contact information and email link to About box,
    so people can reach me easier.
    Suggested by Pau S.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2011/04/19, 00:02:30")]
+[assembly: AssemblyProduct("GitForce built on 2011/10/22, 00:00:04")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.7")]
+[assembly: AssemblyFileVersion("1.0.8")]
