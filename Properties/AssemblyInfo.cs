d39921c0a8f507474e490525b24d94657bf815ef
GitForce <GitForce.Project@gmail.com>
2012-01-19 22:00:23 -0600

    ### Release 1.0.10 (2012/01/19, 21:59:17)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Rewrite of the new repo logic.
    
    Fixes few problems with failed clone removing a directory.
    
    GitRun better handle interruptions
    
    Also initialize ExecReturn variable, fixes certain faults
    
    For the exec run, set a CWD to a process to be run
    
    When terminating, correctly wait for the process exit.
    
    Fixed fault on adding new repos. Initialized Status.
    
    Fixed immediate command execution (F2)
    
    Correctly handle menuMainFetchFromRemote with no repos
    
    Corrected few initial web links.
    
    Be able to open a web link from a command output text pane
    
    Add common OpenWebLink() to ClassUtils
    And changed all instances where we open a weblink.
    Add ToPlainAscii() to clean up strings that we get from stdout
    in the FormRun()
    Fixed PErrorDataReceived() for null strings.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2012/01/18, 00:16:35")]
+[assembly: AssemblyProduct("GitForce built on 2012/01/19, 21:59:17")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.9")]
+[assembly: AssemblyFileVersion("1.0.10")]
