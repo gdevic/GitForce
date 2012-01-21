a8ab2464d22d5f242217cfa0094f573634cf3d2a
GitForce <GitForce.Project@gmail.com>
2012-01-20 22:05:11 -0600

    ### Release 1.0.12 (2012/01/20, 22:04:25)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Changed way to split strings with Environment.Newline
    
    Merge branch 'master' of ssh://github.com:22/gdevic/GitForce
    
    Adding code to check for a new version from GitHub
    
    Massive changes to the error handling
    
    Propagate and use ExecResult structure.
    Changed a lot of '\n' into Environment.NewLine
    Got rid of isLastError thingy

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2012/01/20, 02:08:36")]
+[assembly: AssemblyProduct("GitForce built on 2012/01/20, 22:04:25")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.11")]
+[assembly: AssemblyFileVersion("1.0.12")]
