4eb9f1ba017f4eb387bdc09edaeccac047216050
Goran Devic <GitForce.Project@gmail.com>
2012-02-09 00:48:21 -0600

    ### Release 1.0.15 (2012/02/09, 00:47:05)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Add the setting panes to edit gitconfig files
    
    There are 2 files: a global gitconfig file (in the user
    directory) and a config file per each repo. This change
    adds the widget tabs in settings and repo edit to be
    able to edit those files directly.
    
    Add UserHome global string.
    
    Set the new version check timeout to 4 sec
    
    This prevents ocasional hang when the web site does
    not reply on time.
    
    In the tools menu, add buttons with the help displaying
    and selecting tokens for argument and directory macros:
    
    %r - Root directory of the current git repository
    %u - User name
    %e - User email
    %b - Active branch of the current git repository
    %f - Single selected file in the left pane
    %F - List of selected files in the left pane
    %d - Single selected directory in the left pane
    %D - List of selected directories in the left pane

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2012/01/26, 20:30:45")]
+[assembly: AssemblyProduct("GitForce built on 2012/02/09, 00:47:05")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.14")]
+[assembly: AssemblyFileVersion("1.0.15")]
