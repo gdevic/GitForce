95960f1ce318b03775faff988551463167d4e199
Goran Devic <gitforce.project@gmail.com>
2012-11-17 00:03:44 -0600

    Checking in missing files!

---
+++ FormDownload.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GitForce
+{
+    partial class FormDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelInfo.BackColor = System.Drawing.SystemColors.Info;
+            this.labelInfo.Location = new System.Drawing.Point(12, 9);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(335, 79);
+            this.labelInfo.TabIndex = 0;
+            this.labelInfo.Text = "label1";
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btCancel.Enabled = false;
+            this.btCancel.Location = new System.Drawing.Point(272, 133);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 1;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.BtCancelClick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 100);
+            this.progressBar.Maximum = 100000000;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(335, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 2;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(12, 138);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(78, 13);
+            this.labelProgress.TabIndex = 3;
+            this.labelProgress.Text = "Downloading...";
+            // 
+            // FormDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 168);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.labelInfo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(251, 166);
+            this.Name = "FormDownload";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Download";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelProgress;
+    }
+}
---
+++ FormDownload.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GitForce
+{
+    public partial class FormDownload : Form
+    {
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        private readonly WebClient client = new WebClient();
+        private readonly AutoResetEvent inProgress = new AutoResetEvent(false);
+        private bool isCompleted;
+        private readonly string webPage;
+        private readonly string regEx;
+        private readonly bool mergeWebPath;
+
+        /// <summary>
+        /// Local path and name of the file that was downloaded
+        /// </summary>
+        public string TargetFile { get; private set; }
+
+        public FormDownload(string title, string webPageArg, string regExArg, bool mergeWebPathArg)
+        {
+            InitializeComponent();
+            Text = title;
+            webPage = webPageArg;
+            regEx = regExArg;
+            mergeWebPath = mergeWebPathArg;
+            Work();
+        }
+
+        /// <summary>
+        /// Initiate download of a file through a separate thread
+        /// </summary>
+        private void Work()
+        {
+            labelInfo.Text = "Searching for the latest installer...";
+
+            worker.DoWork += delegate
+            {
+                string webName;
+                if(GetTargetFile(out webName))
+                {
+                    UIThread(() => labelInfo.Text = "Downloading file " + webName);
+                    UIThread(() => btCancel.Enabled = true);
+
+                    // Download the remote file into local temp directory and keep the file name
+                    TargetFile = Path.Combine(Path.GetTempPath(), Path.GetFileName(webName));
+
+                    string response = Download(TargetFile, webName);
+                    if (response == String.Empty)
+                    {
+                        UIThread(() => labelInfo.Text = "SUCCESS");
+                    }
+                    else
+                    {
+                        UIThread(() => labelInfo.Text = response);                        
+                    }
+                }
+                else
+                {
+                    UIThread(() => labelInfo.Text = webName);
+                }
+            };
+            worker.RunWorkerCompleted += WorkerRunWorkerCompleted;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// This is a final function that gets invoked on a job completion
+        /// </summary>
+        void WorkerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Remove temporary file if we did not complete the download
+            if(!isCompleted)
+            {
+                File.Delete(TargetFile);
+                DialogResult = DialogResult.Cancel;
+            }
+            else
+                DialogResult = DialogResult.OK;
+        }
+
+        /// <summary>
+        /// A helper function that lets us update controls in the GUI thread
+        /// </summary>
+        private void UIThread(Action code)
+        {
+            if (InvokeRequired)
+                BeginInvoke(code);
+            else
+                code.Invoke();
+        }
+
+        /// <summary>
+        /// Returns the web path of the target file to download.
+        /// Returns String.Empty if the file could not be itentified.
+        /// </summary>
+        private bool GetTargetFile(out string targetFile)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(webPage);
+                request.Timeout = 4000;
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                StringBuilder file = new StringBuilder();
+                file.Append(reader.ReadToEnd());
+
+                // Find the first executable
+                Regex r = new Regex(regEx, RegexOptions.Compiled);
+                if (r.IsMatch(file.ToString()))
+                {
+                    targetFile = r.Match(file.ToString()).Result("${file}");
+
+                    // Merge the root of the target web page with the name of the file to download
+                    if (mergeWebPath)
+                        targetFile = webPage + targetFile;
+
+                    return true;
+                }
+                targetFile = "Could not find a target file matching a pattern!";
+            }
+            catch (Exception ex)
+            {
+                targetFile = ex.Message;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Download a file from the web path
+        /// </summary>
+        private string Download(string target, string webPath)
+        {
+            try
+            {
+                UriBuilder url = new UriBuilder(webPath);
+
+                client.Proxy = null;
+                client.DownloadProgressChanged += ClientDownloadProgressChanged;
+                client.DownloadFileCompleted += ClientDownloadFileCompleted;
+                client.DownloadFileAsync(url.Uri, target);
+
+                inProgress.WaitOne();        // Wait until download completes or user cancels
+                if (isCompleted==false)
+                    return String.Empty;
+                return "Download has been cancelled.";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Callback by the web client class when the download finish (completed or cancelled)
+        /// </summary>
+        void ClientDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Thread.Sleep(1500);     // A short pause for a better visual flow
+
+            inProgress.Set();
+            isCompleted = !e.Cancelled;
+        }
+
+        /// <summary>
+        /// Callback by the web client when a new piece of file has been incrementally downloaded
+        /// </summary>
+        void ClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            string progress = String.Format("Downloaded {0} of {1} Kb", e.BytesReceived / 1024, e.TotalBytesToReceive / 1024);
+            UIThread(() => labelProgress.Text = progress);
+            UIThread(() => progressBar.Maximum = (int) e.TotalBytesToReceive);
+            UIThread(() => progressBar.Value = (int) e.BytesReceived);
+        }
+
+        /// <summary>
+        /// User clicked on the Cancel button. Abort the download.
+        /// </summary>
+        private void BtCancelClick(object sender, EventArgs e)
+        {
+            client.CancelAsync();
+        }
+    }
+}
