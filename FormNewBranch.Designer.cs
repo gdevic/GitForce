d280818b82ff117ac7574c376fa2364108f1f5a5
GitForce <GitForce.Project@gmail.com>
2013-04-04 12:18:45 -0500

    Usability fixes in the New Branch form
    Setting focus on edit box and Enter to create a branch

--- FormNewBranch.Designer.cs
+++ FormNewBranch.Designer.cs
@@ -55,7 +55,7 @@
             this.listBranches.Location = new System.Drawing.Point(196, 68);
             this.listBranches.Name = "listBranches";
             this.listBranches.Size = new System.Drawing.Size(223, 88);
-            this.listBranches.TabIndex = 6;
+            this.listBranches.TabIndex = 4;
             this.listBranches.SelectedIndexChanged += new System.EventHandler(this.ListBoxSelectedIndexChanged);
             // 
             // textSHA1
@@ -67,7 +67,7 @@
             this.textSHA1.MaxLength = 40;
             this.textSHA1.Name = "textSHA1";
             this.textSHA1.Size = new System.Drawing.Size(223, 20);
-            this.textSHA1.TabIndex = 5;
+            this.textSHA1.TabIndex = 2;
             this.textSHA1.TextChanged += new System.EventHandler(this.TextSha1TextChanged);
             this.textSHA1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBranchNameKeyPress);
             // 
@@ -77,7 +77,7 @@
             this.radioButton5.Location = new System.Drawing.Point(41, 122);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(47, 17);
-            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabIndex = 6;
             this.radioButton5.Tag = "Tag";
             this.radioButton5.Text = "Tag:";
             this.radioButton5.UseVisualStyleBackColor = true;
@@ -89,7 +89,7 @@
             this.radioButton4.Location = new System.Drawing.Point(41, 95);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(102, 17);
-            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabIndex = 5;
             this.radioButton4.Tag = "Remote";
             this.radioButton4.Text = "Remote Branch:";
             this.radioButton4.UseVisualStyleBackColor = true;
@@ -101,7 +101,7 @@
             this.radioButton3.Location = new System.Drawing.Point(41, 68);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(91, 17);
-            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabIndex = 3;
             this.radioButton3.Tag = "Local";
             this.radioButton3.Text = "Local Branch:";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -126,7 +126,7 @@
             this.btCancel.Location = new System.Drawing.Point(365, 229);
             this.btCancel.Name = "btCancel";
             this.btCancel.Size = new System.Drawing.Size(75, 23);
-            this.btCancel.TabIndex = 6;
+            this.btCancel.TabIndex = 2;
             this.btCancel.Text = "Cancel";
             this.btCancel.UseVisualStyleBackColor = true;
             // 
@@ -138,7 +138,7 @@
             this.checkCheckOut.Location = new System.Drawing.Point(56, 38);
             this.checkCheckOut.Name = "checkCheckOut";
             this.checkCheckOut.Size = new System.Drawing.Size(181, 17);
-            this.checkCheckOut.TabIndex = 10;
+            this.checkCheckOut.TabIndex = 1;
             this.checkCheckOut.Text = "Checkout branch after creating it";
             this.checkCheckOut.UseVisualStyleBackColor = true;
             // 
@@ -149,7 +149,7 @@
             this.textBranchName.Location = new System.Drawing.Point(56, 12);
             this.textBranchName.Name = "textBranchName";
             this.textBranchName.Size = new System.Drawing.Size(378, 20);
-            this.textBranchName.TabIndex = 9;
+            this.textBranchName.TabIndex = 0;
             this.textBranchName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBranchNameKeyUp);
             this.textBranchName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBranchNameKeyPress);
             // 
@@ -203,13 +203,14 @@
             this.btCreate.Location = new System.Drawing.Point(284, 229);
             this.btCreate.Name = "btCreate";
             this.btCreate.Size = new System.Drawing.Size(75, 23);
-            this.btCreate.TabIndex = 7;
+            this.btCreate.TabIndex = 3;
             this.btCreate.Text = "Create";
             this.btCreate.UseVisualStyleBackColor = true;
             this.btCreate.Click += new System.EventHandler(this.BtCreateClick);
             // 
             // FormNewBranch
             // 
+            this.AcceptButton = this.btCreate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btCancel;
