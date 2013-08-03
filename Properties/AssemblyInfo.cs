b22fd429f0ffd9aae700d4ae1649a68ba9a24db3
Goran Devic <GitForce.Project@gmail.com>
2013-08-03 11:38:26 -0500

    ### Release 1.0.27 (2013/08/03, 11:36:34)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Skip downloading of diff executable if one was not found
    This only disables that button, everything else is still in place.
    
    Retarget main menu and help to use google sites and SF
    
    Porting to use MSVC 2010 compiler

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2013/04/23, 19:28:45")]
+[assembly: AssemblyProduct("GitForce built on 2013/08/03, 11:36:34")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.25")]
+[assembly: AssemblyFileVersion("1.0.27")]
