400951551febe173cd13e2a482cc8901a8328595
Goran Devic <GitForce.Project@gmail.com>
2015-01-02 08:39:17 -0600

    ### Release 1.0.29 (2015/01/02, 08:37:05)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    - Fixed up msysgit home path and init download code
    - Corrected init KDiff3 d/l path but then disabling auto-download
      until I find a better way to pick a file from that SF site
    - Changed some wording in the diff tool d/l dialog
    
    New project wizard: suggest a project name based on the source
    
    - When cloning, target folders need to exist and be empty
    - Suggest project name based on the directory name of the source
      or the *.git name when cloning from the remote
    
    Do not reset views after creating a new repo
    
    - It feels better when the views don't jump around immediately
      after creating/cloning a repo
    
    Correctly enable/disable project name when cloning
    
    After creating a new repo through 2-step Wizard, open Edit repo
    
    - Gives user the chance to fill in user name and email fields
      and otherwise edit repo settings
    
    Merge branch 'master' of ssh://github.com:22/gdevic/GitForce
    
    Fix behavior of Tools dialog (up/down/...)
    
    Merge branch 'master' of ssh://github.com:22/gdevic/GitForce
    
    Merge branch 'master' of ssh://git.code.sf.net:22/p/gitforce/code
    
    Merge branch 'master' of ssh://github.com:22/gdevic/GitForce
    
    Disable showing revision list for other branches
    
    History for arbitrary branch is broken. For now, we will only
    show the current branch.
    
    Implementing a new GetHomePath()
    Fixes code that loads user's .gitconfig

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2013/08/04, 13:22:01")]
+[assembly: AssemblyProduct("GitForce built on 2015/01/02, 08:37:05")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.28")]
+[assembly: AssemblyFileVersion("1.0.29")]
