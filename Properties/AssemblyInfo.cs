7c7866a81727e66aa02d7926e823a054530c147c
Goran Devic <GitForce.Project@gmail.com>
2015-02-05 00:42:56 -0600

    ### Release 1.0.33 (2015/02/05, 00:41:10)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Changes to the About box
    
    New link to a discussion board
    Minor visual changes
    
    Added Help buttons and links
    
    Added a new section on the website: workspaces
    Added Help button to Recreate Repos dialog
    Added Help button to Tools dialog
    Use ClassHelp for main links so it's consistent
    
    Removed one unused system file menu dialog
    Corrected import private key file dialog
    
    Cleaning up source files: removing trainiling spaces
    
    Better management of workspace load (multiple repos)
    
    New dialog, "Recreate Repos" gives the ability to decide what to do when a
    repo to be loaded is not found locally.
    
    Internally, this change is quite large; watch for regressions!

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2015/01/29, 21:42:22")]
+[assembly: AssemblyProduct("GitForce built on 2015/02/05, 00:41:10")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.32")]
+[assembly: AssemblyFileVersion("1.0.33")]
