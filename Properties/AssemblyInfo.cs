e972feab8911dff12cf7ea0d2b8cb6fb584a0dd8
Goran Devic <GitForce.Project@gmail.com>
2015-03-17 17:22:27 -0500

    ### Release 1.0.35 (2015/03/17, 17:19:50)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Add a menu option to create a new workspace
    This completes a set of useful operations
    on workspaces (sets of git repos)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2015/02/15, 17:11:39")]
+[assembly: AssemblyProduct("GitForce built on 2015/03/17, 17:19:50")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.34")]
+[assembly: AssemblyFileVersion("1.0.35")]
