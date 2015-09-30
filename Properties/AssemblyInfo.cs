7ec4636065bcc620e3ff0d690e288664ab347722
Goran Devic <GitForce.Project@gmail.com>
2015-09-30 10:05:43 -0500

    ### Release 1.0.37 (2015/09/30, 10:05:12)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Added delayed version check timer event
    
    Added a button to list remote hosts to FormSSH
    
    - Added an icon to better represent the list of remote hosts
    - Corrected invocation of FormSSH from FormGitRun class

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2015/09/29, 22:27:37")]
+[assembly: AssemblyProduct("GitForce built on 2015/09/30, 10:05:12")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.36")]
+[assembly: AssemblyFileVersion("1.0.37")]
