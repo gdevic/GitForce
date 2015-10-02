d775adf89fd780227bdf0dd1c7adfb67f5053098
Goran Devic <GitForce.Project@gmail.com>
2015-10-01 21:53:15 -0500

    ### Release 1.0.38 (2015/10/01, 21:52:00)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Corrected the About box
    
    Implement Import workspace
    
    - Import is similar to Load, but it merges the new set of repos
      with the existing one (load will clear the current set first)
    
    Can't send message (and use a DLL) on Mono
    
    Optimize drawing to avoid flicker in the rest of controls
    
    - Avoid flicker in the main listbox
    - Avoid flicker in various tree views
    
    Optimize drawing to avoid flicker in the list of Revisions
    
    Renamed custom listview class
    
    Optimize drawing to avoid flicker in list of repos

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2015/09/30, 10:05:12")]
+[assembly: AssemblyProduct("GitForce built on 2015/10/01, 21:52:00")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.37")]
+[assembly: AssemblyFileVersion("1.0.38")]
