17ec657d4c6fd2501a3e80d4a51e3a8ec9df55a8
Goran Devic <gdevic@yahoo.com>
2016-01-12 14:36:39 -0600

    Requires Python3 to build
    Tested using python 3.5.1

--- Release.py
+++ Release.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Release script for MS Windows
 #
 # * Runs MSBuild to compile GitForce release version
@@ -24,9 +25,9 @@ def IncrementVersion(file, change):
         if line.find("AssemblyFileVersion")>0:
             p = re.compile(r'\W+')
             ver = p.split(line)
-            print 'Major = ', ver[3]
-            print 'Minor = ', ver[4]
-            print 'Build = ', ver[5], ' -> ', int(ver[5])+1
+            print('Major = ', ver[3])
+            print('Minor = ', ver[4])
+            print('Build = ', ver[5], ' -> ', int(ver[5])+1)
             release = ver[3]+'.'+ver[4]+'.'+str(int(ver[5])+1)
             line = line.replace(ver[3]+'.'+ver[4]+'.'+ver[5], release)
 
@@ -34,7 +35,7 @@ def IncrementVersion(file, change):
             now = datetime.datetime.now()
             format = "%Y/%m/%d, %H:%M:%S"
             built = now.strftime(format)
-            print 'Setting the Build date = ', built
+            print('Setting the Build date = ', built)
             line = "[assembly: AssemblyProduct(\"GitForce built on " + built + "\")]\n"
 
         fout.write(line)
@@ -47,12 +48,12 @@ version_file = "Properties/AssemblyInfo.cs"
 changelist_file = "change.txt"
 
 IncrementVersion(version_file, changelist_file)
-print 'Building:'
-subprocess.Popen(["MSBuild.exe", "/t:Rebuild", "/p:Configuration=Release"]).wait()
+print('Building:')
+subprocess.Popen(["MSBuild.exe", "/t:Rebuild", "/p:Configuration=Release"], shell=True).wait()
 
 # Get the summary of changes and append to the subject
 proc = subprocess.Popen(["git", "log", "--format=%B"], stdout=subprocess.PIPE)
-stdout = proc.communicate()[0]
+stdout = proc.communicate()[0].decode('windows-1252')
 stdout = stdout[0:stdout.find("###")]
 
 h = open(changelist_file, "a")
@@ -61,8 +62,6 @@ h.write("SUMMARY OF CHANGES:\n\n")
 h.write(stdout)
 h.close()
 
-print
-print 'Submit ' + version_file 
-print 'using the description in ' + changelist_file
-
-
+print('-------------------------------------------')
+print('Submit ' + version_file)
+print('using the description in ' + changelist_file)
