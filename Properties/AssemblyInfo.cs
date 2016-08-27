b9c9fcaccedc69153b19d1a19fd13c101b6fda80
Goran Devic <gdevic@yahoo.com>
2016-08-27 17:28:53 -0500

    ### Release 1.0.41 (2016/08/27, 17:27:21) ------------------------------------------- SUMMARY OF CHANGES:
    Can't send message (and use a DLL) on Mono
    Fix Linux Repos header sometimes not visible
    Fix Linux Revision header not visible
    WAR for Linux settings fixes restart exception
    
    We used to save one int[] type variable directly but in the settings
    it would embed "<?xml version..." text in the middle of a file which
    would break the Linux mono code
    
    Small change, wrap text
    Fixed sporadic fault when clearing the log pane
    When searching for git.exe, try also 64-bit program folders

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2016/01/17, 15:37:24")]
+[assembly: AssemblyProduct("GitForce built on 2016/08/27, 17:27:21")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.40")]
+[assembly: AssemblyFileVersion("1.0.41")]
