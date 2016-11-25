7968799a7152d9b5a9956660fb6f44ba575f8c06
Goran Devic <gdevic@yahoo.com>
2016-11-24 21:28:26 -0600

    ### Release 1.0.45 (2016/11/24, 21:26:03) ------------------------------------------- SUMMARY OF CHANGES:
    Refresh when syncing a file from Revision History window
    
    Fix a bug when manual refresh was needed on the main pane when syncing
    a file to a previous revision from the Revision History window.
    
    Add View Using context menu to Revision History dialog
    
    Use common function to drive changelist view
    
    Revision History to use ListViewEx instead of ListView
    
    New helper funtion
    
    Describe (view) selected changelist and drive up/down back
    propagation through the host list view
    
    Add Describe Changelist to the Revision History dialog
    
    Add context menu to Revision History dialog
    
    Add Copy SHA submenu to copy selected item SHA to Clipboard
    
    Add ... to Describe Changelist context menu text
    
    Revision History format temp file like P4Win does
    
    When viewing a file (or double-clicking on it) format the temp
    file in the same way P4 does it ("ReadOnly-[rev]-[id]-file").
    
    Revision history fixed a bug when no rev was selected
    
    Add ID to Revision History dialog
    
    More closely resembles Perforce
    
    History dialog, better window test
    
    Show complete path to the file being examined using the OS
    specific path separator, possibly appended with ... if folder
    
    Add Revision History submenu to the Pending Changelists
    
    Corrected Enabled behavior for Pending changelists Diff
    
    Enable only when a file is selected
    
    Add Pending changelist double-click on a file to open it
    
    Extends the newly added menu to edit a file in the Pending
    Changelist pane on the right.
    
    Depending on the selected option, file fill be opened using the
    OS default option or a user defined editor.
    
    Add Edit menu to Pending changelist
    
    Edit menu provides options to edit a file using a default OS
    application for that file type or a set of user edit apps.
    
    This (context) menu replicates one normally shown on the left pane
    
    Selection is enabled only when a single file is selected.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2016/11/06, 19:34:00")]
+[assembly: AssemblyProduct("GitForce built on 2016/11/24, 21:26:03")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.44")]
+[assembly: AssemblyFileVersion("1.0.45")]
