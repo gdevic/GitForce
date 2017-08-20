0fdc53e89c1ad650686774e787f849fa7674b886
Goran Devic <gdevic@yahoo.com>
2017-08-20 06:58:04 -0500

    Improved text to recreate repo form.

--- FormRecreateRepos.Designer.cs
+++ FormRecreateRepos.Designer.cs
@@ -68,9 +68,7 @@
             this.labelHelp.Name = "labelHelp";
             this.labelHelp.Size = new System.Drawing.Size(474, 49);
             this.labelHelp.TabIndex = 0;
-            this.labelHelp.Text = "Some of the repos referenced to by the loaded workspace file could not be found o" +
-    "n the designated paths. Please select repos that are not valid, one by one, and " +
-    "take a desired action.";
+            this.labelHelp.Text = "Some of the repos referenced to by the loaded workspace file could not be found on the designated paths. Please select repos that are not valid and take a desired action.";
             // 
             // list
             // 
