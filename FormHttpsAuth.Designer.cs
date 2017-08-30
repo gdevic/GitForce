76c5f4483e7d33e11f96a95f0f958b97d62c4c54
Goran Devic <gdevic@yahoo.com>
2017-08-30 00:55:22 -0500

    Validator for user name and password in HTTPS Auth

--- FormHttpsAuth.Designer.cs
+++ FormHttpsAuth.Designer.cs
@@ -34,6 +34,7 @@
             this.textPassword = new System.Windows.Forms.TextBox();
             this.btOK = new System.Windows.Forms.Button();
             this.btCancel = new System.Windows.Forms.Button();
+            this.checkShow = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label2
@@ -62,6 +63,7 @@
             this.textUsername.Name = "textUsername";
             this.textUsername.Size = new System.Drawing.Size(227, 20);
             this.textUsername.TabIndex = 16;
+            this.textUsername.TextChanged += new System.EventHandler(this.TextUsernameTextChanged);
             // 
             // textPassword
             // 
@@ -72,11 +74,13 @@
             this.textPassword.Size = new System.Drawing.Size(227, 20);
             this.textPassword.TabIndex = 17;
             this.textPassword.UseSystemPasswordChar = true;
+            this.textPassword.TextChanged += new System.EventHandler(this.TextPasswordTextChanged);
             // 
             // btOK
             // 
             this.btOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btOK.Enabled = false;
             this.btOK.Location = new System.Drawing.Point(167, 68);
             this.btOK.Name = "btOK";
             this.btOK.Size = new System.Drawing.Size(75, 23);
@@ -95,11 +99,23 @@
             this.btCancel.Text = "Cancel";
             this.btCancel.UseVisualStyleBackColor = true;
             // 
+            // checkShow
+            // 
+            this.checkShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkShow.Location = new System.Drawing.Point(15, 72);
+            this.checkShow.Name = "checkShow";
+            this.checkShow.Size = new System.Drawing.Size(104, 24);
+            this.checkShow.TabIndex = 20;
+            this.checkShow.Text = "Show password";
+            this.checkShow.UseVisualStyleBackColor = true;
+            this.checkShow.CheckedChanged += new System.EventHandler(this.CheckShowCheckedChanged);
+            // 
             // FormHttpsAuth
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(335, 103);
+            this.Controls.Add(this.checkShow);
             this.Controls.Add(this.btCancel);
             this.Controls.Add(this.btOK);
             this.Controls.Add(this.textPassword);
@@ -108,7 +124,7 @@
             this.Controls.Add(this.label2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(199, 141);
+            this.MinimumSize = new System.Drawing.Size(351, 141);
             this.Name = "FormHttpsAuth";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
@@ -127,5 +143,6 @@
         private System.Windows.Forms.TextBox textPassword;
         private System.Windows.Forms.Button btOK;
         private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.CheckBox checkShow;
     }
 }
--- FormHttpsAuth.cs
+++ FormHttpsAuth.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace GitForce
 {
@@ -26,6 +27,12 @@ namespace GitForce
         }
 
         /// <summary>
+        /// Return user name and password fields as typed in the control
+        /// </summary>
+        public string Username => textUsername.Text.Trim();
+        public string Password => textPassword.Text.Trim();
+
+        /// <summary>
         /// Form constructor
         /// </summary>
         public FormHttpsAuth()
@@ -40,6 +47,33 @@ namespace GitForce
         private void FormHttpsAuth_FormClosing(object sender, FormClosingEventArgs e)
         {
             ClassWinGeometry.Save(this);
+        }
+
+        /// <summary>
+        /// Show or hide password field
+        /// </summary>
+        private void CheckShowCheckedChanged(object sender, System.EventArgs e)
+        {
+            textPassword.UseSystemPasswordChar = !checkShow.Checked;
+        }
+
+        /// <summary>
+        /// Validator for the user name field: must start with a letter and ...
+        /// </summary>
+        private void TextUsernameTextChanged(object sender, System.EventArgs e)
+        {
+            string s = textUsername.Text.Trim();
+
+            Regex r = new Regex("^[a-zA-Z][a-zA-Z0-9_]*$");
+            btOK.Enabled = r.IsMatch(s);
+        }
+
+        // This is a really lame validator for the password field
+        // We simply want to avoid some 'dangerous' characters, including spaces
+        private void TextPasswordTextChanged(object sender, System.EventArgs e)
+        {
+            string s = textPassword.Text.Trim();
+            btOK.Enabled = (s.Length > 0) && s.IndexOfAny(@" \/".ToCharArray()) == -1;
         }
     }
 }
