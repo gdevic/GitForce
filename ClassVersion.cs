5999d92d02d10a51c960328b3e72e8d979fcf890
Goran Devic <gdevic@yahoo.com>
2018-02-24 09:07:05 -0600

    Rewrote new version check code

--- ClassVersion.cs
+++ ClassVersion.cs
@@ -38,6 +38,7 @@ namespace GitForce
         /// This should really be interlocking, but volatile will do with bools.
         /// </summary>
         public volatile bool NewVersionAvailable = false;
+        public volatile bool MessageAlreadySent = false;
 
         /// <summary>
         /// Thread handle for function that checks for a new version.
@@ -68,10 +69,10 @@ namespace GitForce
             altRequest.Timeout = 5000;
 
             // Create and start the thread to check for the new version
-            threadCheck = new Thread(ThreadVersionCheck);
+            threadCheck = new Thread(() => ThreadVersionCheck(request));
             threadCheck.Start();
 
-            altThreadCheck = new Thread(AltThreadVersionCheck);
+            altThreadCheck = new Thread(() => ThreadVersionCheck(altRequest));
             altThreadCheck.Start();
 #endif
         }
@@ -83,17 +84,18 @@ namespace GitForce
         {
 #if !DEBUG
             // Abort the web request
-            request.Abort();
-            // Abort the thread. First give it a nice nudge and then simply abort it.
-            threadCheck.Join(1);
-            threadCheck.Abort();
+            if (request != null) request.Abort();
+            if (altRequest != null) altRequest.Abort();
+            // Abort the threads. First give it a nice nudge and then simply abort them.
+            threadCheck.Join(10); threadCheck.Abort();
+            altThreadCheck.Join(10); altThreadCheck.Abort();
 #endif
         }
 
         /// <summary>
         /// This function is run on a separate thread. It checks for a new version.
         /// </summary>
-        private void ThreadVersionCheck()
+        private void ThreadVersionCheck(WebRequest request)
         {
             // A lot of things can go wrong here...
             try
@@ -106,45 +108,7 @@ namespace GitForce
                         answer.Append(reader.ReadToEnd());
                     }
                 }
-
-                // Search for the version string, for example:
-                // [assembly: AssemblyFileVersion(&quot;1.0.11&quot;)]
-                string sPattern = @"GitForce-(?<major>\d+).(?<minor>\d+).(?<build>\d+).exe";
-                Regex r = new Regex(sPattern, RegexOptions.Compiled);
-                if (r.IsMatch(answer.ToString()))
-                {
-                    // Get the version numbers from the latest version checked in github
-                    string major = r.Match(answer.ToString()).Result("${major}");
-                    string minor = r.Match(answer.ToString()).Result("${minor}");
-                    string build = r.Match(answer.ToString()).Result("${build}");
-                    int webMajor = Convert.ToInt32(major);
-                    int webMinor = Convert.ToInt32(minor);
-                    int webBuild = Convert.ToInt32(build);
-
-                    // Get the current version numbers
-                    string[] current = GetVersion().Split('.');
-                    int thisMajor = Convert.ToInt32(current[0]);
-                    int thisMinor = Convert.ToInt32(current[1]);
-                    int thisBuild = Convert.ToInt32(current[2]);
-
-                    // Compare two versions and set flag if the current one is less
-                    if (thisMajor < webMajor)
-                        NewVersionAvailable = true;
-                    else
-                        if (thisMinor < webMinor)
-                            NewVersionAvailable = true;
-                        else
-                            if (thisBuild < webBuild)
-                                NewVersionAvailable = true;
-
-                    // By now we have log window availabe, so print out what's going on
-                    if (NewVersionAvailable)
-                        App.PrintStatusMessage("**** A new version of GitForce is available! ****", MessageType.NewVersion);
-                    else
-                        App.PrintStatusMessage("Version check OK - this version is up-to-date.", MessageType.General);
-                }
-                else
-                    App.PrintStatusMessage("Version check: Unable to match pattern!", MessageType.Error);
+                ParseNewVersionResponse(answer);
             }
             catch (Exception ex)
             {
@@ -153,32 +117,49 @@ namespace GitForce
         }
 
         /// <summary>
-        /// This function is run on a separate thread. It checks for a new version.
+        /// Parses the response string from new version check sites
         /// </summary>
-        private void AltThreadVersionCheck()
+        private void ParseNewVersionResponse(StringBuilder answer)
         {
-            // A lot of things can go wrong here...
-            try
+            if (MessageAlreadySent) // Print only one message for multiple site's checks
+                return;
+            // Search for the version string, for example:
+            // [assembly: AssemblyFileVersion(&quot;1.0.11&quot;)]
+            string sPattern = @"GitForce-(?<major>\d+).(?<minor>\d+).(?<build>\d+).exe";
+            Regex r = new Regex(sPattern, RegexOptions.Compiled);
+            if (r.IsMatch(answer.ToString()))
             {
-                StringBuilder answer = new StringBuilder();
-                using (WebResponse response = altRequest.GetResponse())
-                {
-                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                    {
-                        answer.Append(reader.ReadToEnd());
-                    }
-                }
-                // TODO...
-                if (answer.ToString().Length > 0)
-                {
-                    Thread.Sleep(2000); // Make sure this message is shown after all other messages in the view pane
-                    App.PrintStatusMessage(answer.ToString(), MessageType.NewVersion);
-                }
+                // Get the version numbers from the latest version checked in github
+                string major = r.Match(answer.ToString()).Result("${major}");
+                string minor = r.Match(answer.ToString()).Result("${minor}");
+                string build = r.Match(answer.ToString()).Result("${build}");
+                int webMajor = Convert.ToInt32(major);
+                int webMinor = Convert.ToInt32(minor);
+                int webBuild = Convert.ToInt32(build);
+
+                // Get the current version numbers
+                string[] current = GetVersion().Split('.');
+                int thisMajor = Convert.ToInt32(current[0]);
+                int thisMinor = Convert.ToInt32(current[1]);
+                int thisBuild = Convert.ToInt32(current[2]);
+
+                // Compare two versions and set flag if the current one is less
+                if (thisMajor < webMajor)
+                    NewVersionAvailable = true;
+                else if (thisMinor < webMinor)
+                    NewVersionAvailable = true;
+                else if (thisBuild < webBuild)
+                    NewVersionAvailable = true;
+
+                // By now we have log window availabe, so print out what's going on
+                if (NewVersionAvailable)
+                    App.PrintStatusMessage("**** A new version of GitForce is available! ****", MessageType.NewVersion);
+                else
+                    App.PrintStatusMessage("Version check OK - this version is up-to-date.", MessageType.General);
+                MessageAlreadySent = true;
             }
-            catch (Exception ex)
-            {
-                App.PrintStatusMessage("Version check: " + ex.Message, MessageType.Error);
-            }
+            else
+                App.PrintStatusMessage("Version check: Unable to match pattern!", MessageType.Error);
         }
     }
 }
