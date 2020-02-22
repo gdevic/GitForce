c9a29f29aa7a274486c9bf12a086c9735ee6851f
Goran Devic <gdevic@yahoo.com>
2020-02-21 23:24:53 -0600

    ### Release 1.0.57 (2020/02/21) ------------------------------------------- SUMMARY OF CHANGES:
    Fix errors on long repo clones
    Give the job 60 sec to cleanly finish
    Remove ESC[ + single character sequence from the git output
    Minor whitespace and EOL cleanup
    Add --repo command line option
    --repo="path" Open a specified existing git repo upon start.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2019/11/03")]
+[assembly: AssemblyProduct("GitForce built on 2020/02/21")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.56")]
+[assembly: AssemblyFileVersion("1.0.57")]
