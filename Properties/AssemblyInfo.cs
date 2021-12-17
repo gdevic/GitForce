f74bf252c408d42bc325f298560f08e7b0942c49
Goran Devic <gdevic@yahoo.com>
2021-12-17 13:50:42 -0600

    ### Release 1.0.59 (2021/12/17)
    -------------------------------------------
    SUMMARY OF CHANGES:
    
    Class Exec, set StandardOutputEncoding to UTF8
    
    set property StandardOutputEncoding to System.Text.Encoding.UTF8 in
    StartInfo, for the correct display of symbols.
    
    Fixes issue #34
    
    Delete branch add option to remove tracking of a remote branch

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2020/12/31")]
+[assembly: AssemblyProduct("GitForce built on 2021/12/17")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.58")]
+[assembly: AssemblyFileVersion("1.0.59")]
