0873632abbdf54b20d78f04d09861a5ae726a9f4
Goran Devic <gdevic@yahoo.com>
2026-02-27 18:24:19 -0600

    ### Release 1.0.66 (2026/02/27) ------------------------------------------- SUMMARY OF CHANGES:
    Show uninitialized submodules so the user can discover and initialize them
    
    Fix status line file messages
    
    Change submodule "Update" to "Reset"
    
    View, selection: trim trailing separators to match status dictionary keys
    
    Move submodule's Add to Repos from Info dialog to context menu
    
    Remove Sync URL option on a submodule
    
    Used when URL in .gitmodules changes. Simply run "git submodule sync" instead.
    
    Warn when switching to detached HEAD
    
    Also add option in Settings/Files to disable this warning
    
    Add submodule info dialog
    
    Repo scanning avoid system directories and dot folders
    
    Better submodule lifecycle flow
    
    Better remote URL validation
    
    Allow basic git operations on a submodule
    
    Add basic submodule handling

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A Visual Git Frontend Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitForce")]
-[assembly: AssemblyProduct("GitForce built on 2025/11/05")]
+[assembly: AssemblyProduct("GitForce built on 2026/02/27")]
 [assembly: AssemblyCopyright("Written by Goran Devic, released under GPLv3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Effective application version number
-[assembly: AssemblyFileVersion("1.0.65")]
+[assembly: AssemblyFileVersion("1.0.66")]
